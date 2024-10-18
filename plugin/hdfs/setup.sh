1aad4d52e2b275fe5e993ff254e98ca622ffa740
Chen Ding <1017472599@qq.com>
2024-10-18 19:23:40 +0800

    Update setup.sh

--- plugin/hdfs/setup.sh
+++ plugin/hdfs/setup.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2148
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/"
-export HADOOP_HOME="/home/dc/hadoop-3.4.0"
+export HADOOP_HOME="/home/{user}/hadoop-3.4.0"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JAVA_HOME/lib/server:$JAVA_HOME/lib:$HADOOP_HOME/lib/native
 
 export CLASSPATH=`$HADOOP_HOME/bin/hadoop classpath --glob`
